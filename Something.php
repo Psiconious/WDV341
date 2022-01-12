367b6b205d59ab3a75927bbda02aa3ef596bdb14
Trever Cluney <chadcloney@yahoo.com>
2022-01-11 19:35:44 -0800

    Add files via upload
    first upload

---
+++ Something.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>WDC341 Intro Into PHP</title>
+</head>
+<body>
+	<h1>WDV341 intro PHP</h1>
+	<h2>Hello world</h2>
+	<?php
+		echo "<h3>Hello World! from PHP</h3>";
+	?>
+
+</body>
+</html>
